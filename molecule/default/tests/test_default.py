ea1d9d8656f38e7b899d6f901d46f521b7829a0b
Dennis Conrad <dennis.conrad@sainsburys.co.uk>
2018-02-06 12:31:01 +0000

    [GITHUB-1] Adds initial role

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,18 +1,33 @@
 import os
-
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE'],
+).get_hosts('all')
 
 
-def test_hosts_file(host):
-    # Assert /etc/hosts exists,...
-    f = host.file('/etc/hosts')
-    assert f.exists
-    # ...is owned by the user root,...
-    assert f.user == 'root'
-    # ...and owned by the group root.
-    assert f.group == 'root'
+def test_inspector_files(host):
+    vars = host.ansible(
+        'include_vars',
+        'file=../../defaults/main.yml',
+    ).get('ansible_facts')
 
-# See http://testinfra.readthedocs.io/ for guidance on writing testinfra tests.
+    agent_dest = vars.get('sansible_amazon_inspector_agent_dest')
+
+    installer = host.file(agent_dest)
+    assert installer.exists
+    assert installer.user == 'root'
+    assert installer.group == 'root'
+    assert installer.mode == 493  # 0755 oct
+
+    sig = host.file(agent_dest + '.sig')
+    assert sig.exists
+    assert sig.user == 'root'
+    assert sig.group == 'root'
+    assert sig.mode == 420  # 0644 oct
+
+    key = host.file(agent_dest + '.key')
+    assert key.exists
+    assert key.user == 'root'
+    assert key.group == 'root'
+    assert key.mode == 420  # 0644 oct
